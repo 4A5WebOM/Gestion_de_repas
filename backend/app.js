984847e540a3ad88240e7f0081b773b97097f48a
Jad <jadsabb6@gmail.com>
2024-05-06 22:38:16 -0400

    FAIT : Connection du backend a mongoDB

--- backend/app.js
+++ backend/app.js
@@ -1,1 +1,26 @@
+require('dotenv').config();
+const express = require('express');
+const mongoose = require('mongoose');
 
+const app = express();
+
+// Middleware
+app.use(express.json());
+app.use((req, res, next) => {
+    console.log(req.path, req.method);
+    console.log(req.body);
+    next();
+});
+
+// Connection à la base de données MongoDB
+mongoose
+    .connect(process.env.MONGO_URI)
+    .then(() => {
+        console.log('Connecté à la base de données MongoDB');
+        app.listen(process.env.PORT, () => {
+            console.log(`Serveur est connecté au port ${process.env.PORT}`);
+        });
+    })
+    .catch((err) => {
+        console.error(err);
+    });
