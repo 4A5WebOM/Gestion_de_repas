51903d676566865348a6047474c24361b3535e21
Alex Soliman <alexsoliman2002@gmail.com>
2024-05-08 12:37:41 -0400

    FAIT:<Creation du composant MealPlanList, qui permet de stocker des mealPlans>

--- frontend/src/components/MealPlan/MealPlanList.jsx
+++ frontend/src/components/MealPlan/MealPlanList.jsx
@@ -1,2 +1,20 @@
 import React, { useEffect, useState } from "react";
 import "./MealPlanList.css";
+
+const MealPlanList = ({ mealPlans }) => {
+  return (
+    <div>
+      <h2>Meal Plans</h2>
+      <ul>
+        {mealPlans.map((mealPlan) => (
+          <li key={mealPlan.id}>
+            <h3>{mealPlan.title}</h3>
+            <p>{mealPlan.description}</p>
+          </li>
+        ))}
+      </ul>
+    </div>
+  );
+};
+
+export default MealPlanList;
