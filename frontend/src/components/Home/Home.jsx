239fa2011d6b9c5b3673eca16bc09296607d18b0
Jad <jadsabb6@gmail.com>
2024-05-08 16:46:18 -0400

    FAIT : Commencer le Home page

--- frontend/src/components/Home/Home.jsx
+++ frontend/src/components/Home/Home.jsx
@@ -1,10 +1,23 @@
 import React from "react";
-
+import "./Home.css";
 
 export default function Home() {
-    return (
-        <div>
-        <h1>Home</h1>
-        </div>
-    );
+  const scrollDown = () => {
+    window.scrollTo({ top: window.innerHeight, behavior: "smooth" });
+  };
+
+  return (
+    <>
+      <div className="home-background">
+        <h1>Bienvenue</h1>
+        <button onClick={scrollDown} className="home-scroll-down-button">
+          Entrer
+        </button>
+      </div>
+      <div className="home-latest-recipes">
+        <h1>Les dernières recettes</h1>
+        
+      </div>
+    </>
+  );
 }
