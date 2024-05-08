60714e0dbba929513ba7f6115de435060e3df365
Alex Soliman <alexsoliman2002@gmail.com>
2024-05-08 17:28:17 -0400

    FAIT:MealPlanListPage.jsx

---
+++ frontend/src/MealPlanListPage/MealPlanListPage.jsx
@@ -0,0 +1,13 @@
+import React from "react";
+import MealPlanList from "../components/MealPlan/MealPlanList";
+
+const MealPlanListPage = () => {
+  return (
+    <div>
+      <h1>Liste des plans de repas</h1>
+      <MealPlanList />
+    </div>
+  );
+};
+
+export default MealPlanListPage;
