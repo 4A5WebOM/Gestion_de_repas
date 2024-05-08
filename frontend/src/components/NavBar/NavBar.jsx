c729d64370c31295ce7d31d8cd3c8a7924a988db
Jad <jadsabb6@gmail.com>
2024-05-08 14:12:45 -0400

    FAIT : changement de la position des element du navbar

--- frontend/src/components/NavBar/NavBar.jsx
+++ frontend/src/components/NavBar/NavBar.jsx
@@ -11,6 +11,29 @@ function NavBar() {
   return (
     <nav className="navbar">
       <ul>
+        <li>
+          <Link to="/">Home</Link>
+        </li>
+        <li className="dropdown">
+          <span>Recettes</span>
+          <div className="dropdown-content">
+            <Link to="/RecipeListPage">Liste des recettes</Link>
+            {user && (
+              <Link to="/RecipeFormPage">Ajouter/modifier une recette</Link>
+            )}
+          </div>
+        </li>
+        <li className="dropdown">
+          <span>Plans de Repas</span>
+          <div className="dropdown-content">
+            <Link to="/MealPlanListPage">Liste des plans de repas</Link>
+            {user && (
+              <Link to="/MealPlanFormPage">
+                Ajouter/modifier un plan de repas
+              </Link>
+            )}
+          </div>
+        </li>
         {!user && (
           //desactive au moment qu'un utililisateur est connecte
           <>
@@ -22,26 +45,6 @@ function NavBar() {
             </li>
           </>
         )}
-        <li className="dropdown">
-          <span>Nos Recettes</span>
-
-          <div className="dropdown-content">
-            <Link to="/RecipeListPage">Liste des recettes</Link>
-            <Link to="/RecipeDetailPage">Détails des recettes</Link>
-            <Link to="/RecipeFormPage">Ajouter/modifier une recette</Link>
-          </div>
-        </li>
-        <li className="dropdown">
-          <span>Plans de Repas</span>
-
-          <div className="dropdown-content">
-            <Link to="/MealPlanListPage">Liste des plans de repas</Link>
-            <Link to="/MealPlanDetailPage">Détails des plans de repas</Link>
-            <Link to="/MealPlanFormPage">
-              Ajouter/modifier un plan de repas
-            </Link>
-          </div>
-        </li>
         {user && (
           //active au moment qu'un utilisateur est connecte
           <>
@@ -49,19 +52,10 @@ function NavBar() {
               <button onClick={logout}>Deconnexion</button>
             </li>
             <li>
-              <Link to="/ProfilePage">Profil</Link>
-            </li>
-            <li>
-              <Link to="/FavoritesPage">Favoris</Link>
-            </li>
-            <li>
-              <Link to="/GroceryListPage">Liste d'Épicerie</Link>
+              <span>Bonjour {user.username}</span>
             </li>
           </>
         )}
-        <li>
-          <Link to="/">Home</Link>
-        </li>
       </ul>
     </nav>
   );
