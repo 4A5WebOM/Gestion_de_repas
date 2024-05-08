2eea8ba112641af847fabe5355b8f6557daee9da
Jad <jadsabb6@gmail.com>
2024-05-08 02:47:43 -0400

    FAIT : Modifier le navBar pour inclure si le user est connecter

--- frontend/src/components/NavBar/NavBar.jsx
+++ frontend/src/components/NavBar/NavBar.jsx
@@ -3,27 +3,39 @@ import "./NavBar.css";
 import { Link } from "react-router-dom";
 import "../Login/Login.jsx";
 import "../RecipeListPage/RecipeListPage.jsx";
+import { useAuthContext } from "../../hooks/useAuthContext.js";
 
 
 function NavBar() {
+  const { user } = useAuthContext();
+
   return (
     <nav className="navbar">
       <ul>
-      <li>
-          <Link to="/login" >
-              Connexion
-            </Link>
-          </li>
-          <li>
-            <Link to="/RecipeListPage" >
-              Nos recettes
-            </Link>
-          </li>
-          <li>
-            <Link to="/signup">
-              Inscription
-            </Link>
-          </li>
+        {!user && (
+          <>
+            <li>
+              <Link to="/login" >
+                Connexion
+              </Link>
+            </li>
+            <li>
+              <Link to="/signup" >
+                Inscription
+              </Link>
+            </li>
+          </>
+        )}
+        <li>
+          <Link to="/RecipeListPage" >
+            Nos recettes
+          </Link>
+        </li>
+        <li>
+          <Link to="/" >
+            Home
+          </Link>
+        </li>
       </ul>
     </nav>
   );
