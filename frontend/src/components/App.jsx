e4791d49c50dd46b5c2b4f1ba4dec39d34d2e878
Alex Soliman <alexsoliman2002@gmail.com>
2024-05-09 10:12:50 -0400

    FAIT:<Ajout du MealPlanListPage a l'app>

--- frontend/src/components/App.jsx
+++ frontend/src/components/App.jsx
@@ -1,18 +1,22 @@
 import React from "react";
-import { BrowserRouter as Router, Route, Routes, Navigate } from "react-router-dom";
+import {
+  BrowserRouter as Router,
+  Route,
+  Routes,
+  Navigate,
+} from "react-router-dom";
 import { useAuthContext } from "../hooks/useAuthContext.js";
 
 // Pages et Components
-import Login from  "../components/Login/Login.jsx";
+import Login from "../components/Login/Login.jsx";
 import RecipeListPage from "../components/RecipeListPage/RecipeListPage.jsx";
 import NavBar from "./NavBar/NavBar";
-import SignUp from "../components/SignUp/SignUp.jsx"; 
+import SignUp from "../components/SignUp/SignUp.jsx";
 import Home from "../components/Home/Home.jsx";
 import Profil from "./Profil/Profil.jsx";
-
+import MealPlanListPage from "../MealPlanListPage/MealPlanListPage.jsx";
 function App() {
   const { user } = useAuthContext();
-
 
   return (
     <Router>
@@ -20,14 +24,24 @@ function App() {
         <NavBar />
         <Routes>
           <Route path="/" element={<Home />} />
-          <Route path="/login" element={!user ? <Login /> : <Navigate to="/" />} />
+          <Route
+            path="/login"
+            element={!user ? <Login /> : <Navigate to="/" />}
+          />
           <Route path="/RecipeListPage" element={<RecipeListPage />} />
-          <Route path="/signup"  element={!user ? <SignUp /> : <Navigate to="/" />} />
-          <Route path="/profile" element={user ? <Profil /> : <Navigate to="/login" />} />
+          <Route path="/MealPlanListPage" element={<MealPlanListPage />} />
+          <Route
+            path="/signup"
+            element={!user ? <SignUp /> : <Navigate to="/" />}
+          />
+          <Route
+            path="/profile"
+            element={user ? <Profil /> : <Navigate to="/login" />}
+          />
         </Routes>
       </div>
     </Router>
   );
-};
+}
 
 export default App;
